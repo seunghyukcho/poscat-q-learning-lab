3a15114899c7a381d53c8b342a4730addbb89a68
shhj1998 <shhj1998@postech.ac.kr>
2019-07-03 06:36:34 +0900

    add sample code

--- src/player.cpp
+++ src/player.cpp
@@ -2,11 +2,13 @@
 
 void Player::train(Enemy *enemy)
 {
-
+	char state[4][4];
+	// Replace the under code with yours
+	
+	enemy->action(state); // it will return the actions of enemy if the state is given.
 }
 
 int Player::selectAction(char board[][4])
 {
-
 	return 1;
 }
